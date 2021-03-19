86a78af1ab34c6ba52c09642d84d2d35f1c565fb
chrisimi <mittermaier.c@a1.net>
2021-03-19 18:19:20 +0100

    added list for entries

--- PasswordManager.Web/Controllers/HomeController.cs
+++ PasswordManager.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using PasswordManager.Web.Models;
+﻿using PasswordManager.Domain;
+using PasswordManager.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,22 @@ namespace PasswordManager.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<Entry> _entries = new List<Entry>()
+        {
+            new Domain.Entry()
+            {
+                Key = "google.com",
+                Email = "chrisi@gmail.com",
+                Password = "pwd1"
+            },
+            new Domain.Entry()
+            {
+                Key = "willtreffen.at",
+                Email ="essa@hak.at",
+                Password = "burger123"
+            }
+        };
+
         public ActionResult Index()
         {
             return View();
@@ -34,15 +51,7 @@ namespace PasswordManager.Web.Controllers
 
             return View(new PandelModel()
             {
-                Entries = new List<Domain.Entry>()
-                {
-                    new Domain.Entry()
-                    {
-                        Key = "google.com",
-                        Email = "chrisi@gmail.com",
-                        Password = "pwd1"
-                    }
-                }
+                Entries = _entries
             });
         }
     }
