10c827891341baac3e8377fd05c6b072c0690355
Jakob Mayer <MAYER.Jakob@hak-liezen.at>
2021-06-08 13:12:08 +0200

    Code auf File gestreamt

---
+++ PasswordManager.XML/UserEntries.cs
@@ -0,0 +1,17 @@
+﻿using PasswordManager.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PasswordManager.XML
+{
+    [XmlRoot("UserEntries")]
+    public class UserEntries
+    {
+        [XmlArrayItem("Entry")]
+        public List<Entry> Items { get; set; }
+    }
+}
